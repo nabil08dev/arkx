2beb00a64709b5b541f2083fc086d0547670deed
nabil sabbani <nabilbac2019@gmail.com>
2025-07-23 14:53:21 +0100

    day5

---
+++ days/the library challenge/challenge.js
@@ -0,0 +1,94 @@
+const books = require("./books.json");
+
+function priceOfBook(bookName) {
+  for(i=0;i<=books.length;i++){
+    if(books[i].title===bookName){
+      return books[i].price
+    }
+  }
+
+}
+console.log(priceOfBook("The Alchemist"))
+
+function affordableBooks(budget) {
+  let result=[];
+  for(i=0;i<books.length;i++){
+    if(books[i].price<=budget){
+      result.push(books[i].title);
+    }
+  }
+  return result
+}
+console.log(affordableBooks(8))
+
+
+function findBookByGenre(genre) {
+  let result=[]
+  for (i=0;i<books.length;i++){
+    for (j=0;j<genre.length;j++){
+    if(books[i].genres[j]==genre){
+
+      result.push(books[i].title)
+    }
+  }}
+    return result
+}
+console.log(findBookByGenre("Religion"))
+
+
+function groupByGenre() {
+  const groupedBooks = {};
+  for (let bookKey in books) {
+    const book = books[bookKey];
+    for (let genre of book.genres) {
+      if (!groupedBooks[genre]) {
+        groupedBooks[genre] = [];
+      }
+      groupedBooks[genre].push(book.title);
+    }
+  }
+  return groupedBooks;
+}
+
+console.log(groupByGenre());
+
+
+function sortBooksByPrice() {
+ const sortedBooks = [];
+  for (let key in books) {
+    sortedBooks.push(books[key]);
+  }
+  for (let i = 0; i < sortedBooks.length - 1; i++) {
+    for (let j = i + 1; j < sortedBooks.length; j++) {
+      if (sortedBooks[i].price > sortedBooks[j].price) {
+        const temp = sortedBooks[i];
+        sortedBooks[i] = sortedBooks[j];
+        sortedBooks[j] = temp;
+      }
+    }
+  }
+  return sortedBooks;}
+console.log(sortBooksByPrice())
+/*
+(function main() {
+  try {
+    if (priceOfBook("The Alchemist") !== 9.49) {
+      throw new Error("priceOfBook is not working properly.");
+    }
+    if (affordableBooks(10).length !== 6) {
+      throw new Error("affordableBooks is not working properly.");
+    }
+    if (findBookByGenre("Fiction").length !== 7) {
+      throw new Error("findBookByGenre is not working properly.");
+    }
+    if (Object.keys(groupByGenre()).length !== 30) {
+      throw new Error("groupByGenre is not working properly.");
+    }
+    if (sortBooksByPrice()[0].price !== 5.99) {
+      throw new Error("sortBooksByPrice is not working properly.");
+    }
+    console.log("All tests passed successfully.");
+  } catch (error) {
+    console.log(error);
+  }
+})();*/
